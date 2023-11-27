5f8277f4c499eacda0c13a8b6c7fccf92a64d185
Komal Kaur <komalkaurstaymenity@gmail.com>
2023-11-26 16:15:55 -0800

    created auth comp page, with sign in form comp

--- src/ components/sign-in-form/sign-in-form.component.jsx
+++ src/ components/sign-in-form/sign-in-form.component.jsx
@@ -1,48 +1,48 @@
 import { useState } from "react";
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
-import "./sign-up-form.styles.scss";
+import "./sign-in-form.styles.scss";
 import {
-  createAuthUserWithEmailAndPassword,
+  signInWithGooglePopUp,
   createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
 } from "../../utils/firebase/firebase.utils";
 import { Form } from "react-router-dom";
 
 const defaultFormFields = {
-  displayName: "",
   email: "",
   password: "",
-  confirmPassword: "",
 };
 
-const SignUpForm = () => {
+const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
-  const { displayName, email, password, confirmPassword } = formFields;
+  const { email, password } = formFields;
 
   const resetFormField = () => {
     setFormFields(defaultFormFields);
   };
 
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopUp();
+    await createUserDocumentFromAuth(user);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (password !== confirmPassword) {
-      alert("your password does not match");
-      return;
-    }
-
     try {
-      const { user } = await createAuthUserWithEmailAndPassword(
+      const response = await signInAuthUserWithEmailAndPassword(
         email,
         password
       );
-      await createUserDocumentFromAuth(user, { displayName });
+      console.log(response);
       resetFormField();
     } catch (error) {
-      if (error.code === "auth/email-already-in-use") {
-        alert("Cannot create user, email is already in use");
-      } else {
-        console.log("user creation encountered an error", error);
+      console.log(error);
+      if (error.code === "auth/invalid-login-credentials") {
+        alert(
+          "Invalid login credentials! Please check your email and password."
+        );
       }
     }
   };
@@ -54,17 +54,9 @@ const SignUpForm = () => {
   };
   return (
     <div className="sign-up-container">
-      <h2>Don't have an account?</h2>
+      <h2>Already have an account?</h2>
       <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
-        <FormInput
-          label="Display Name"
-          type="text"
-          required
-          onChange={handleChange}
-          name="displayName"
-          value={displayName}
-        />
         <FormInput
           label="Email"
           type="email"
@@ -81,20 +73,15 @@ const SignUpForm = () => {
           name="password"
           value={password}
         />
-        <FormInput
-          label="Confirm Password"
-          type="password"
-          required
-          onChange={handleChange}
-          name="confirmPassword"
-          value={confirmPassword}
-        />
-        <Button buttonType="inverted" type="submit">
-          Sign Up
-        </Button>
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+            Google sign in
+          </Button>
+        </div>
       </form>
     </div>
   );
 };
 
-export default SignUpForm;
+export default SignInForm;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import NavigationBar from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 const Shop = () => {
   return <h1>Shop</h1>;
@@ -13,7 +13,7 @@ const App = () => {
       <Route path="/" element={<NavigationBar></NavigationBar>}>
         <Route index element={<Home></Home>}></Route>
         <Route path="/shop" element={<Shop></Shop>}></Route>
-        <Route path="/signIn" element={<SignIn></SignIn>}></Route>
+        <Route path="/auth" element={<Authentication></Authentication>}></Route>
       </Route>
     </Routes>
   );
