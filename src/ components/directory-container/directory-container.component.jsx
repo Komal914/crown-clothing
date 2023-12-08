5bed2ccb653c4f2a3dcfb2d58aabe91fdbb3fcdb
Komal Kaur <komalkaurstaymenity@gmail.com>
2023-12-07 18:15:55 -0800

    dynamic routing added to categories

--- src/ components/directory-container/directory-container.component.jsx
+++ src/ components/directory-container/directory-container.component.jsx
@@ -1,12 +1,12 @@
+import DirectoryItem from "../directory-item/directory-item.component";
 import "./directory-container.styles.scss";
-import CategoryItem from "../category-item/category-item.component";
 
 const DirectoryContainer = ({ categories }) => {
   return (
     <div className="directory-container">
       {categories.map((category) => {
         return (
-          <CategoryItem key={category.id} category={category}></CategoryItem>
+          <DirectoryItem key={category.id} category={category}></DirectoryItem>
         );
       })}
     </div>
